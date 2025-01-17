ddc36b48ed3d02391f748cec2357c2bcb7c56490
Roel <10514742+rboonzaijer@users.noreply.github.com>
2025-01-17 08:20:50 +0100

    fixes #36

---
+++ tests/Integration/KernelTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Tests\Integration;
+
+use App\Kernel;
+use PHPUnit\Framework\Attributes\CoversClass;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+#[CoversClass(Kernel::class)]
+class KernelTest extends KernelTestCase
+{
+    protected function setUp(): void
+    {
+        self::bootKernel();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    public function testProjectDir(): void
+    {
+        $this->assertNotEmpty(
+            self::getContainer()->getParameter('kernel.project_dir')
+        );
+        $this->assertEquals(
+            dirname(dirname(__DIR__)),
+            self::getContainer()->getParameter('kernel.project_dir')
+        );
+    }
+}
