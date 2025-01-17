3045e3171131d2024693354de571621b3d13d0dd
Roel <10514742+rboonzaijer@users.noreply.github.com>
2025-01-17 09:51:22 +0100

    DateHelper

---
+++ tests/Unit/Helpers/DateHelperTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+// https://symfony.com/doc/current/testing.html#integration-tests
+// The KernelTestCase also makes sure your kernel is rebooted for each test.
+// This assures that each test is run independently from each other.
+
+namespace App\Tests\Unit\Services;
+
+use App\Helpers\DateHelper;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+// https://docs.phpunit.de/en/11.5/code-coverage.html#targeting-units-of-code
+
+#[CoversClass(DateHelper::class)]
+class DateHelperTest extends TestCase
+{
+    public function testValidDates(): void
+    {
+        $dateHelper = new DateHelper();
+        $this->assertTrue($dateHelper->isValidDate('2025-01-01 10:15:58'));
+        $this->assertTrue($dateHelper->isValidDate('2025-01-01 10:15'));
+        $this->assertTrue($dateHelper->isValidDate('2025-01-01'));
+        $this->assertTrue($dateHelper->isValidDate('2024-02-29'));
+        $this->assertTrue($dateHelper->isValidDate('2025-02-28'));
+    }
+
+    public function testInvalidDates(): void
+    {
+        $dateHelper = new DateHelper();
+        $this->assertFalse($dateHelper->isValidDate('2025-13-01'));
+    }
+}
