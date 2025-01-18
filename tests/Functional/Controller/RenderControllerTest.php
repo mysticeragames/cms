61c9b617dbc87ddeea3e396b0357c3c82860ac1c
Roel <10514742+rboonzaijer@users.noreply.github.com>
2025-01-18 14:24:57 +0100

    test specific paths for rendering

--- tests/Functional/Controller/RenderControllerTest.php
+++ tests/Functional/Controller/RenderControllerTest.php
@@ -12,6 +12,7 @@ namespace App\Tests\Functional\Controller;
 use App\Controller\Render\RenderController;
 use App\Tests\Base\BaseFunctionalTestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\Routing\RouterInterface;
 
@@ -20,23 +21,62 @@ use Symfony\Component\Routing\RouterInterface;
 #[CoversClass(RenderController::class)]
 class RenderControllerTest extends BaseFunctionalTestCase
 {
-    public function testIndex(): void
+    public static function additionProvider(): array
+    {
+        return [
+            '' => [ '', 'Home Page', 'Homepage'],
+            '/about' => [ '/about', 'About Page', 'About'],
+            '/news' => [ '/news', '404 Not Found', 'News'],
+            '/news/post 1' => [ '/news/post 1', 'Post 1', 'My Post 1'],
+            '/news/post%201' => [ '/news/post%201', 'Post 1', 'My Post 1'],
+        ];
+    }
+
+    #[DataProvider('additionProvider')]
+    public function testIndex(string $path, string $expectedH1Text, string $expectedAText): void
     {
         $client = static::createClient();
+        $client->followRedirects(true);
         $container = self::getContainer();
 
         /** @var RouterInterface $router */
         $router = $container->get('router');
         //$routeCollection = $router->getRouteCollection();
 
-        $url = $router->generate('render', ['site' => $this->getTestSiteName(), 'path' => '']);
-        //$this->assertEquals('/render/phpunit-test-site/', $url);
+        // Be very specific about the path for this test:
+        // Try: /render/site/path
+        // Try: /render/site/path/
+        // Try: /render/site/path?
+        // Try: /render/site/path/?
+        // etc...
+        $suffixes = [
+            '',
+            '/',
+            '?',
+            '/?',
+            '?v=1',
+            '/?v=1',
+            '#',
+            '/#',
+            '?#',
+            '/?#',
+            '?v=1#',
+            '/?v=1#',
+        ];
 
-        /** @var Crawler $response */
-        $response = $client->request('GET', $url);
+        foreach ($suffixes as $suffix) {
+            $url = $router->generate('render', [
+                'site' => $this->getTestSiteName(),
+                'path' => '' // By adding it here, it will always append a slash, so for this test, don't do it...
+            ]);
+            $url = rtrim($url, '/') . $path . $suffix;
 
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Home Page');
-        $this->assertAnySelectorTextContains('a', 'Homepage');
+            /** @var Crawler $response */
+            $response = $client->request('GET', $url . $suffix);
+
+            $this->assertResponseIsSuccessful();
+            $this->assertSelectorTextContains('h1', $expectedH1Text);
+            $this->assertAnySelectorTextContains('a', $expectedAText);
+        }
     }
 }
